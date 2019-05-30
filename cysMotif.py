bba5c8707a29787b455ec981098b35e468f56fd7
Jeff Cole <coleti16@students.ecu.edu>
2019-05-30 16:46:28 -0400

    describes cys pattern

--- cysMotif.py
+++ cysMotif.py
@@ -1,64 +1,81 @@
-from itertools import groupby
-from itertools import (takewhile,repeat)
-
-def fasta_iter(fasta_name):
-    """
-    given a fasta file. yield tuples of header, sequence
-    """
-    fh = open(fasta_name)
-    # ditch the boolean (x[0]) and just keep the header or sequence since
-    # we know they alternate.
-    faiter = (x[1] for x in groupby(fh, lambda line: line[0] == ">"))
-    for header in faiter:
-        # drop the ">"
-        header = header.next()[1:].strip()
-        # join all sequence lines to one.
-        seq = "".join(s.strip() for s in faiter.next())
-        yield header, seq
-
-
+import re
+import json
+import pandas as pd
 import sys
-def getOptionValue(option):
-    optionPos = [i for i, j in enumerate(sys.argv) if j == option][0]
-    optionValue = sys.argv[optionPos + 1]
-    return optionValue
-if "-i" in sys.argv:
-    fileName = getOptionValue("-i")
-else:
-    print "\nplease specify input file name using -i <file_name> \n"
-    sys.exit()
 
-sequence_iterator = fasta_iter(fileName)
-
-
-cysDict = {}
-with open("Id.CysMotif.txt","w") as out:
-    out.write("ID\tCysMotif\n")
-    for ff in sequence_iterator:
-        headerStr, seq = ff
-
-        firstC = False
-        cysMotif = ""
-        number = 0
-        for i in seq:
-
-            if i == "C":
-                if firstC == False:
-                    firstC=True
-                    number = 0
-                    cysMotif+="C"
+#test_file = "test/SPIDER.knottin.fa"
+def fa2dict(fa):
+    fasta_dict = {}
+    with open(fa) as f:
+        header = ""
+        for line in f:
+            if line.strip() != '':
+                if line[0] == ">":
+                    header = line.strip().strip(">")
+                    fasta_dict[header] = ""
                 else:
-                    cysMotif+="-"+str(number)+"-C"
-                    number = 0
-            else:
-                number+=1
-        if cysMotif not in cysDict:
-            cysDict[cysMotif] = 1
-        else:
-            cysDict[cysMotif] +=1
-        writeMe = headerStr.split()[0]+"\t"+ cysMotif  +'\n'
-        out.write(writeMe)
-with open("CysMotifAbundance.txt","w") as out:
-    out.write("Abundance\tCysMotif\n")
-    for i in cysDict.keys():
-        out.write(str(cysDict[i])+"\t"+i+"\n")
+                    fasta_dict[header] += line.strip()
+    return fasta_dict
+
+def findICK(fa_dict,json=True):
+    cys_dict = {}
+    pattern_num = 1
+    pattern_dict ={}
+    cys_pd_dict = {"count":[],"pattern_num":[],"pattern":[],"header":[]}
+    for header in fa_dict:
+        seq = fa_dict[header]
+        cys_count = seq.count("C")
+        if cys_count != 0:
+            cys_positions = [pos for pos, char in enumerate(seq) if char == "C"]
+            cys_pattern = [x - (1+cys_positions[i - 1]) for i, x in enumerate(cys_positions)][1:]
+            cys_seqs = [seq[cys_positions[i - 1]+1:x] for i,x in enumerate(cys_positions)][1:]
+            cys_motif = ''.join(["-C-"+str(x - (1+cys_positions[i - 1])) for i, x in enumerate(cys_positions)][1:]+["-C"]).strip('-')
+            if cys_count not in cys_dict:
+                cys_dict[cys_count] = {}
+            if cys_motif not in cys_dict[cys_count]:
+                cys_dict[cys_count][cys_motif] = {}
+                if cys_motif not in pattern_dict:
+                    pattern_dict[cys_motif] = pattern_num
+                    pattern_num += 1
+            cys_dict[cys_count][cys_motif][header] = cys_seqs
+            #print(cys_count,cys_motif,header)
+            cys_pd_dict["count"].append(cys_count)
+            cys_pd_dict["pattern"].append(cys_motif)
+            cys_pd_dict["pattern_num"].append(pattern_dict[cys_motif])
+            cys_pd_dict["header"].append(header)
+    if json:
+        return cys_dict
+    else:
+        return pd.DataFrame.from_dict(cys_pd_dict)
+
+
+#spider_knottin_fa = fa2dict("test/SPIDER.knottin.fa")
+# spider_knottin_pd = findICK(spider_knottin_fa,json=False)
+
+try:
+    input_file = "test/P.nigriventer.ick.fa" # replace with sys input
+except:
+    input_file = sys.argv[1]
+    
+json_out = True # replace with sys input
+
+input_fa = fa2dict(input_file)
+if json_out:
+    input_dict = findICK(input_fa)
+    for cys_num in input_dict:
+        for pattern in input_dict[cys_num]:
+            general_pattern = pattern.replace("-0-","")
+            general_pattern = general_pattern.replace("-1-","-X-")
+            general_pattern = re.sub(r'[0-9]+', '', general_pattern)
+            general_pattern = general_pattern.replace("--","-")
+
+            for header in input_dict[cys_num][pattern]:
+                print(header,general_pattern,pattern)
+
+else:
+    input_pd = findICK(input_fa,json=False)
+# print(spider_knottin_pd)
+# print(P_nigriventer_dict)
+
+
+        # print(pattern,len(P_nigriventer_dict[cys_num][pattern]))
