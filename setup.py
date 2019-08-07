5ccf69a08223624f9ef22c7863ba500b1ca0dc93
Jeff Cole <coleti16@students.ecu.edu>
2019-08-07 17:47:18 -0400

    add Cys_num column in classify

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='cluck',
-      version='0.1.2',
+      version='0.1.3',
       description='Command Line Utility for Cysteine Knot peptides',
       url='http://github.com/tijeco/cluck',
       author='Flying Circus',
